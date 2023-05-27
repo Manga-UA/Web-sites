be416ace5c7def532dbf8ddb5ea6d451962b6902
Oduvanchik43 <oduvanchikyan@gmail.com>
2023-05-27 23:10:55 +0300

    add tailwind.css and TitleManga

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -7,7 +7,8 @@ import { MANGA_ROUTE } from '../utils/consts';
 import { Context } from '../index';
 const AppRouter = () => {
 	const {user} = useContext(Context);
-	console.log(user);
+	const {title} = useContext(Context);
+	// console.log(title.titles[0].id_title);
 	return (
 		// Працює таким чином, що вказавши йому декілька маршрутів
 		// якщо не один з них не відпрацює або буде вказаний не коректно,
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserManga from './manga/UserManga';
+import TittleManga from './manga/TitleManga';
+import './dist/output.css'
 export const Context = createContext(null);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Context.Provider value={{
-			user: new UserManga()
+			user: new UserManga(),
+			title: new TittleManga(),
 		}}>
 			<App />
 		</Context.Provider>
