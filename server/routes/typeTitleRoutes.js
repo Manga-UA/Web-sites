85bdbad9a492ab730e65dc1d203bb991b96e3596
DamaskinUa <kozurslava@gmail.com>
2023-05-27 16:11:35 +0300

    test

--- server/routes/typeTitleRoutes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router 
-const typeTitleController = require('../controllers/TypeTitleController')
-const checkRole = require('../middleware/checkRoleMiddleware')
-
-router.post('/',checkRole(3),typeTitleController.create)
-router.get('/',typeTitleController.getAll)
-
-module.exports = router
